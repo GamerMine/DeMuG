d67db0049f8c3c9bb5d50feac1a215125b9011f8
gamermine <gamermine@dwightstudio.fr>
2025-05-03 02:21:27 +0200

    Starting to implement opcodes
    - Adding all main opcodes

--- demug-gui/src/main.rs
+++ demug-gui/src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
     //let demug = libdemug::Demug::init();
     
-    let mut a: u16 = 0x9100;
-    let orig = a;
-    
-    a = a.wrapping_add(0xFF00);
-    add16_flag(orig, a);
-    
-    println!("{:#X}", a);
-}
+    let mut a: u8 = 0x35;
 
-fn add16_flag(base_value: u16, result_value: u16) {
-    if result_value < base_value {
-        println!("HALF CARRY + CARRY");
-    } else if result_value >> 12 > base_value >> 12 {
-        println!("HALF CARRY");
-    }
+    a = !a;
+
+    println!("{:#X}", a);
 }
